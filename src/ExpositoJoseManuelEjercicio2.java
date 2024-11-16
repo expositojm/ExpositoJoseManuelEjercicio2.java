9b9663d1e9b35f42e4ef079bbaa389d9ecc2383c
expositojm <josema.xpo@gmail.com>
2024-11-16 12:45:38 +0100

    Pedir caracter controlando que solo se introduzca 1

---
+++ src/ExpositoJoseManuelEjercicio2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ExpositoJoseManuelEjercicio2 {
+    public static void main(String[] args) {
+        char pattern;
+        int size;
+        String input;
+
+
+        Scanner scanner = new Scanner(System.in);
+        //pedir el caracter (solo 1 caracter)
+        do {
+            System.out.println("TABLERO PERSONALIZADO");
+            System.out.print("Introduce el caracter que quieres para tu tablero: ");
+            input = scanner.nextLine();
+
+            if (input.length() != 1) {
+                System.out.println("Solo uno caracter por favor");
+            }
+        } while (input.length() != 1);
+
+
+
+
+    }
+}
