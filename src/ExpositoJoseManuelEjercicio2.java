23c98bd82581cbe7df8c8162e91f2b61a25c1547
expositojm <josema.xpo@gmail.com>
2024-11-16 13:58:17 +0100

    Imprimir el resultado

--- src/ExpositoJoseManuelEjercicio2.java
+++ src/ExpositoJoseManuelEjercicio2.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class ExpositoJoseManuelEjercicio2 {
     public static void main(String[] args) {
-        char pattern;
-        int size;
-        String input;
+        int size;        // tamaño elegido por el usuario
+        String pattern; //caracter elegido por usuario es string para controlar que solo introduzca 1
 
 
         Scanner scanner = new Scanner(System.in);
@@ -11,12 +10,12 @@ public class ExpositoJoseManuelEjercicio2 {
         do {
             System.out.println("TABLERO PERSONALIZADO");
             System.out.print("Introduce el caracter que quieres para tu tablero: ");
-            input = scanner.nextLine();
+            pattern = scanner.nextLine();
 
-            if (input.length() != 1) {
+            if (pattern.length() != 1) {
                 System.out.println("Solo uno caracter por favor");
             }
-        } while (input.length() != 1);
+        } while (pattern.length() != 1);
 
         // Solicitar el tamaño
 
@@ -25,12 +24,35 @@ public class ExpositoJoseManuelEjercicio2 {
             size = scanner.nextInt();
         } while (size < 1 || size > 15);
 
-        //imprimir barra sepa horizonal
+        //imprimir barra separadora horizonal
+        System.out.println("Aqui tienes tu tablero personalizado:");
         for (int i = 0; i < 8 * (size + 1) + 1; i++) {
             System.out.print("-");
         }
         System.out.println();
 
+        for (int row = 0; row < 8; row++) {
+            for (int height = 0; height < size; height++) {
+                for (int column = 0; column < 8; column++) {
+                    System.out.print("|");
+                    for (int width = 0; width < size; width++) {
+                        if ((row + column) % 2 == 1) {                      //si es par imprime pattern
+                            System.out.print(pattern);
+                        } else {
+                            System.out.print(" ");                          //si no imprime en blanco
+                        }
+                    }
+                }
+                System.out.println("|");
+            }
+            //imprimir barra separadora horizonal
+            for (int i = 0; i < 8 * (size + 1) + 1; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+
+        
 
     }
 }
